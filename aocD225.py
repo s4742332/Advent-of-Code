3a21dffda86c384c1024bd539f857103a059f948
s4742332 <s4m4ts4m@gmail.com>
2025-12-02 20:29:10 +1000

    working code for part 1

--- aocD225.py
+++ aocD225.py
@@ -3,7 +3,7 @@ To extract the Data into aoc.txt Steps in obsidian
 """
 code = 0
 
-with open("testD2.txt", "r") as file:
+with open("IDs.txt", "r") as file:
     allcode = file.read() # Read file
     all_IDs = allcode.split(',') #split ID Ranges
     for id in all_IDs: #For each ID Range
@@ -11,7 +11,7 @@ with open("testD2.txt", "r") as file:
         full_ID = range(int(first_ID), (int(last_ID)+1)) #Range between IDs
         for full_number in full_ID: #For each inidividual number in the range of IDS
                 full_number = str(full_number) #conver to string
-                if len(full_number)%2 != 0: #if the entire string is an odd length, true double ups don't exist
+                if len(full_number)%2 != 0: #if the entire string is odd in length
                     continue
                 # all examples are double around the mid point, this also prevents double ups (e.g. 8x1 and 4x2 in a 22222222 ID)
                 mid = len(full_number) //2
