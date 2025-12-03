9554c2e4516f9b2af1075aed1ca449a2bd3622ee
s4742332 <s4m4ts4m@gmail.com>
2025-12-03 17:18:49 +1000

    got both part 1 and part 2 tests working at the same time

--- aocD325.py
+++ aocD325.py
@@ -1,12 +1,17 @@
 output = 0
+first_part = 0
 
 with open("testD3.txt", "r") as file:
     allbanks = file.read().split() # Read file, convert to list of str
     alljoltage = []
+    first_part_alljoltage = []
     for bank in allbanks: #for each battery bank
         bank_joltage = []
+        first_part_bank_joltage = []
         for a in range(0, len(bank)):
             for b in range(a+1, len(bank)):
+                first_part_joltage = str(bank[a]+bank[b])
+                first_part_bank_joltage.append(int(first_part_joltage))
                 for c in range(b+1, len(bank)):
                     for d in range(c+1, len(bank)):
                         for e in range(d+1, len(bank)):
@@ -19,12 +24,15 @@ with open("testD3.txt", "r") as file:
                                                     for l in range(k+1, len(bank)):
                                                         joltage = str(bank[a]+bank[b]+bank[c]+bank[d]+bank[e]+bank[f]+bank[g]+bank[h]+bank[i]+bank[j]+bank[k]+bank[l])
                                                         bank_joltage.append(int(joltage))
-        alljoltage.append(sorted(bank_joltage, reverse=True)[0]) #append the highest bank voltage, then turn to string to cut off the remining length
-        print(sorted(bank_joltage, reverse=True)[0], "For bank:", bank)
+        alljoltage.append(sorted(bank_joltage, reverse=True)[0])
+        first_part_alljoltage.append(sorted(first_part_bank_joltage, reverse=True)[0]) #append the highest bank voltage, then turn to string to cut off the remining length
+        print(sorted(first_part_bank_joltage, reverse=True)[0], sorted(bank_joltage, reverse=True)[0], "For bank:", bank)
         bank_joltage.clear()
 
+    first_part = sum(first_part_alljoltage)
     output = sum(alljoltage)
-    print("Answer:", output)
+    print("Answer Part 1:", first_part)
+    print("Answer Part 2:", output)
 
               
 
