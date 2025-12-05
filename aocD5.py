6a4b3f413591ee8192a902724974e45d8cac3519
s4742332 <s4m4ts4m@gmail.com>
2025-12-05 19:25:38 +1000

    part 2 added and working for test

--- aocD5.py
+++ aocD5.py
@@ -13,11 +13,17 @@ def get_ranges(everything):
                     ranges.append(int(x[0:(y)]))
                     ranges.append(int(x[(y+1):len(x)]))
                 else: continue
-    print("Ranges:", ranges)
-    print("IDs:", IDs)
+    #print("Ranges:", ranges)
+    #print("IDs:", IDs)
     return ranges, IDs
 
-
+def total_possible_fresh(ranges):
+    all_ranges = []
+    for i in range(1, len(ranges),2):
+        for k in range(int(ranges[i-1]), int(ranges[i]+1)):
+            if k not in all_ranges:
+                all_ranges.append(k)
+    return len(all_ranges)
 
 with open("testD5.txt", "r") as file:
     everything = file.read().split('\n') #seperate by liens
@@ -28,26 +34,27 @@ with open("testD5.txt", "r") as file:
     list_spoiled = []
 
     for id in IDs:
-        print(id)
+        #print(id)
         k = int(id)
         place = IDs.index(id)
         for i in range(1, len(ranges),2):
             if int(ranges[i-1]) <= k < (int(ranges[i])+1):
                 fresh +=1
-                print("Fresh:", int(ranges[i-1]),int(ranges[i]),":", id)
+                #print("Fresh:", int(ranges[i-1]),int(ranges[i]),":", id)
                 list_fresh.append(id)
                 #IDs.pop(place)
                 break
             else:
-                print("Spoiled:",int(ranges[i-1]),int(ranges[i]),":", id)
+                #print("Spoiled:",int(ranges[i-1]),int(ranges[i]),":", id)
                 continue
         if id not in list_fresh:
             list_spoiled.append(id)
     #print(hasduplicates(sorted(list_fresh))) proves no duplicates in list
-    print("All IDs:", IDs)
-    print("All Fresh:", list_fresh)
-    print("All Spoiled:", list_spoiled)
-    print("Total Fresh", fresh)
+    #print("All IDs:", IDs)
+    #print("All Fresh:", list_fresh)
+    #print("All Spoiled:", list_spoiled)
+    #print("Total Fresh", fresh)
+    print(total_possible_fresh(ranges))
             
 
             
